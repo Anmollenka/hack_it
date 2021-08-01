311507a99df99dea53dbfe7346d0d2a9d0b2ad01
Ayushmina <ayushminanayak20@gmail.com>
2021-08-01 10:39:17 -0700

    Authentication

--- client/src/components/LoginPage/Signup.jsx
+++ client/src/components/LoginPage/Signup.jsx
@@ -8,6 +8,43 @@ export default function Login() {
  const switchTosignin=()=>{
      history.push('/');
  }
+ const history=useHistory();
+ const [user,setUser]=useState(
+     {
+         name:"",email:"",contact:"",password:""
+     }
+ );
+
+let name,value;
+const handleInputs =(e)=>{
+console.log(e);
+name=e.target.name;
+value=e.target.value;
+setUser({...user,[name]:value});
+}
+
+const PostData=async (e)=>{
+ e.preventDefault();
+ const{name,email,contact,password}=user;
+ const res=await fetch("/signup",{
+     method:"POST",
+     headers:{
+         "Content-type":"application/json"
+     },
+     body:JSON.stringify({
+         name,email,contact,password
+     })
+ });
+ const data=await res.json();
+ if(data.status===422 || !data ){
+     window.alert("Invalid Registration");
+ }
+ else{
+     console.log("Succesful registration");
+     history.push("/");
+ }
+
+}
   return (
     <>
     <Navbar />
@@ -23,44 +60,44 @@ export default function Login() {
             <label htmlFor="name">
               <h3>Name</h3>
             </label>
-            <input
-              type="text"
-              name="name"
-              placeholder="Ram"
-            ></input>
+            <input type="text" name="name" id="name" autoComplete="off"
+                                value={user.name}
+                                onChange={handleInputs}
+                                    placeholder="Your Name"/>
           </div>
           <div className="form-group">
           <label htmlFor="email">
             <h3>Email Address</h3>
           </label>
-          <input
-            type="email"
-            name="email"
-            placeholder="you@example.com"
-          ></input>
+          <input type="text" name="email" id="email" autoComplete="off"
+                                value={user.email}
+                                onChange={handleInputs}
+                                    placeholder="Your Email"/>
         </div>
         <div className="form-group">
         <label htmlFor="contact">
-          <h3>Email Address</h3>
+          <h3>Contact</h3>
         </label>
-        <input
-          type="number"
-          name="contact"
-          placeholder="12345678"
-        ></input>
+        <input type="number" name="contact" id="contact" autoComplete="off"
+                                value={user.contact}
+                                onChange={handleInputs}
+                                    placeholder="Your Phone"/>
       </div>
           <div className="form-group">
             <label htmlFor="password">
               <h3>Password</h3>
             </label>
-            <input
-              type="password"
-              name="password"
-              placeholder="Enter 6 characters or more"
-            ></input>
+            <input type="password" name="password" id="password" autoComplete="off"
+            value={user.password}
+            onChange={handleInputs}
+                placeholder="Your Password"/>
           </div>
           <div className="footer">
-            <button>SIGNUP</button>
+          <div className="form-group form-button">
+          <input type="submit" name="signup" id="signup" className="form-submit"
+          onClick={PostData}
+              value="register"/>
+      </div>
             <div class="border">
               <hr></hr>OR<hr></hr>
             </div>
--- server/router.js
+++ server/router.js
@@ -1,8 +1,77 @@
 const express=require("express")
 const router=express.Router()
+const mongoose=require('mongoose');
+const bcrypt=require('bcrypt');
+const DB='mongodb+srv://Ayushmina:1234@cluster0.thczm.mongodb.net/Room?retryWrites=true&w=majority'
+mongoose.connect(DB,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true,
+    useFindAndModify:false
+}).then(()=>{
+    console.log('connection is successfull');
+}).catch((err)=>{
+    console.error('No connection');
 
+});
+const User= require('./model/userSchema');
+router.use(express.json());
 router.get("/",(req,res)=>{
     res.send("Server is running").status(200);
 })
+router.post('/login',async(req,res)=>{
+    try{
+        const{email,password}=req.body;
+        if(!email || !password){
+            return res.status(400).json({error:"plz"});
+        }
+
+        const userLogin = await doctor.findOne({email:email});
+        if(userLogin){
+            const isMatch=await bcrypt.compare(password,userLogin.password);
+        if(!isMatch){
+            res.status(400).json({error:"user error"});
+        }
+        else{
+            res.json({message:"user signed in"});
+        }
+        }
+        else{
+            res.json({message:"user not signed in"});
+        }
+        
+
+    }
+    catch(err){
+        console.log(err);
+    }
+});
+router.post('/signup',async(req ,res)=>{
+    const{name,email,contact,password}=req.body;
+    if(!name || !email || !contact || !password  ){
+        res.status(422).json({error:"please fill up"});
+    }
+   /* res.json({msg:"success"});*/
+    console.log(name);
+    try{
+        const userExist=await User.findOne({email:email});
+        if(userExist){
+            res.status(422).json({error:"please fill new email  up"});
+        }
+        const user=new User({name,email,contact,password});
+        // generate salt to hash password
+        const salt = await bcrypt.genSalt(10);
+        console.log(salt);
+        // now we set user password to hashed password
+         user.password = await bcrypt.hash(user.password, salt);
+        const userRegister=await user.save();
+        if(userRegister){
+            res.status(201).json({message:"user saved"});
+        }
+    }
+    catch(err){
+        console.log(err);
+    }
+});
 
 module.exports=router;
